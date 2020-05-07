8af35f7b2d6ed1931db26067953a5846db63efa8
MattIPv4 <me@mattcowley.co.uk>
2020-05-07 21:24:18 +0100

    Some initial stats

---
+++ components/whos_participating.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="section whos-participating">
+    <div class="container">
+      <h1 class="title">
+        Who's participating
+      </h1>
+
+      <p>
+        Anyone and everyone can participate in Project Blurple, and many already have!
+      </p>
+
+      <p>
+        Already, over <code>31 thousand members</code> have joined the official Project Blurple server, with an amazing
+        <code>1.8k Blurple users</code> who have already updated their Discord avatar to use Blurple colors as part of
+        the celebration.
+      </p>
+
+      <p>
+        Alongside the Blurple users, <code>290+ Discord servers</code> have already listed themselves in the Project
+        Blurple server with their custom Blurple-themed icons.
+      </p>
+
+      <p>
+        Since launching the 2020 celebration on the 7th May, members of the official Project Blurple server have posted
+        a massive <code>58k+ messages</code> so far, chatting with each other and sharing their love for all
+        things Discord &amp; Blurple.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'WhosParticipating',
+  };
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <Header />
     <GetInvolved />
     <Colors />
+    <WhosParticipating />
   </div>
 </template>
 
@@ -10,12 +11,14 @@
   import Header from '../components/header.vue';
   import GetInvolved from '../components/get_involved.vue';
   import Colors from '../components/colors.vue';
+  import WhosParticipating from '../components/whos_participating.vue';
 
   export default {
     components: {
       Header,
       GetInvolved,
       Colors,
+      WhosParticipating,
     },
   };
 </script>
