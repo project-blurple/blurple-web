aec9dffbc8fb941fe4fd5c024107dc6e029da8d9
MattIPv4 <me@mattcowley.co.uk>
2020-05-08 13:30:07 +0100

    Add full footer content

--- components/footer.vue
+++ components/footer.vue
@@ -14,10 +14,32 @@
           </p>
         </li>
 
-        <!-- TODO: reddit, twitter -->
+        <li>
+          <p>
+            We also have a community subreddit at
+            <code><a href="https://www.reddit.com/r/projectblurple">reddit.com/r/projectblurple</a></code> that you can
+            join, where you can share Blurple images, memories from past events and using Discord, as well as anything
+            else you can think of!
+          </p>
+        </li>
+
+        <li>
+          <p>
+            Get the latest updates from Project Blurple in your timeline by following
+            <code><a href="https://twitter.com/BlurpleProject">@BlurpleProject</a></code> on Twitter. The Project
+            Blurple executive team all have access to this account and share updates about the celebrations on it.
+          </p>
+        </li>
       </ol>
 
-      <!-- TODO: not affiliated with Discord, copyright PB & created by me -->
+      <p>
+        <small>
+          Project Blurple is not affiliated with <a href="https://discord.com">Discord Inc</a>.
+          <br>
+          &copy; 2020 Project Blurple. All rights reserved.
+          Created by <a href="https://mattcowley.co.uk">v4</a>.
+        </small>
+      </p>
     </div>
   </footer>
 </template>
