9010e0159e00f3fb968340b4728b79fd832b7527
MattIPv4 <me@mattcowley.co.uk>
2020-05-10 10:26:21 +0100

    Update stats

--- components/whos_participating.vue
+++ components/whos_participating.vue
@@ -12,22 +12,29 @@
           </p>
 
           <p>
-            Already, over <code>43 thousand members</code> have joined the official Project Blurple server, with an
-            amazing
-            <code>4.8k Blurple users</code> who have already updated their Discord avatar to use Blurple colors as part
-            of
-            the celebration.
+            Already, over <code>46 thousand members</code> have joined the <a
+              href="https://projectblurple.com/discord"
+              target="_blank"
+              rel="noopener"
+            >official Project Blurple server</a>, with an amazing <code>5.6k Blurple users</code> who have
+            already updated their Discord avatar to use Blurple colors as part of the celebration.
           </p>
 
           <p>
-            Alongside the Blurple users, <code>600+ Discord servers</code> have already listed themselves in the Project
-            Blurple server with their custom Blurple-themed icons.
+            Alongside the Blurple users, <code>660+ Discord servers</code> have already listed themselves in the <a
+              href="https://projectblurple.com/discord"
+              target="_blank"
+              rel="noopener"
+            >Project Blurple server</a> with their custom Blurple-themed icons.
           </p>
 
           <p>
-            Since launching the 2020 celebration on May 7th, members of the official Project Blurple server have posted
-            a massive <code>215k+ messages</code> so far, chatting with each other and sharing their love for all
-            things Discord &amp; Blurple.
+            Since launching the 2020 celebration on May 7th, members of the <a
+              href="https://projectblurple.com/discord"
+              target="_blank"
+              rel="noopener"
+            >official Project Blurple server</a> have posted a massive <code>281k+ messages</code> so far, chatting with
+            each other and sharing their love for all things Discord &amp; Blurple.
           </p>
         </div>
         <div class="column is-one-quarter is-hidden-tablet-only is-hidden-mobile">
