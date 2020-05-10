21d03bcc8b3f787c34dc569c75e303a2accdc1f1
MattIPv4 <me@mattcowley.co.uk>
2020-05-10 10:07:30 +0100

    Add some more partner icons

---
+++ components/images/servers3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="columns images">
+    <div class="column is-half">
+      <img src="../../assets/img/servers/r-starwars.png" title="r/StarWars" alt="">
+      <img src="../../assets/img/servers/the-witcher.png" title="The Witcher" alt="">
+      <img src="../../assets/img/servers/discord-boats.png" title="Discord Boats" alt="">
+      <img src="../../assets/img/servers/frogs-blurple-world.png" title="Frogs Blurple World" alt="">
+    </div>
+    <div class="column is-half">
+      <img src="../../assets/img/servers/r-iphone.png" title="r/iPhone" alt="">
+      <img src="../../assets/img/servers/scp-secret-laboratory.png" title="SCP: Secret Laboratory" alt="">
+      <img src="../../assets/img/servers/shreks-swamp.png" title="Shrek's Swamp" alt="">
+      <img src="../../assets/img/servers/sounds-world.png" title="Sound's World" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ServerImages3',
+  };
+</script>
--- components/what_is_project_blurple.vue
+++ components/what_is_project_blurple.vue
@@ -1,57 +1,69 @@
 <template>
   <section class="section what-is-project-blurple">
     <div class="container">
-      <h1 class="title">
-        What is Project Blurple?
-      </h1>
+      <div class="columns">
+        <div class="column is-three-quarters is-full-tablet">
+          <h1 class="title">
+            What is Project Blurple?
+          </h1>
 
-      <p>
-        If you've got this far, you might have some idea, but what exactly is Project Blurple?
-      </p>
+          <p>
+            If you've got this far, you might have some idea, but what exactly is Project Blurple?
+          </p>
 
-      <blockquote>
-        <p>
-          Project Blurple is simple at its core, we're here to celebrate Discord's birthday each year with the community.
-          We do this by encouraging folks to participate in our Discord server, chatting, entering giveaways and
-          joining in with any events we run there.
-        </p>
-      </blockquote>
+          <blockquote>
+            <p>
+              Project Blurple is simple at its core, we're here to celebrate Discord's birthday each year with the
+              community. We do this by encouraging folks to participate in our Discord server, chatting, entering
+              giveaways and joining in with any events we run there.
+            </p>
+          </blockquote>
 
-      <p>
-        The other big thing we do is asking those celebrating with us to make their avatars and server icons Blurple
-        colored. Blurple is Discord's main brand color and by having everyone change their images to use a color scheme
-        based on this, we can show our appreciation for Discord outside the Project Blurple server.
-      </p>
+          <p>
+            The other big thing we do is asking those celebrating with us to make their avatars and server icons Blurple
+            colored. Blurple is Discord's main brand color and by having everyone change their images to use a color
+            scheme based on this, we can show our appreciation for Discord outside the Project Blurple server.
+          </p>
 
-      <p>
-        Discord's birthday is on May 13th, so Project Blurple normally launches roughly a week before to get the
-        community hyped and ready to celebrate, with the main partying happening on May 13th. Things wrap up quickly
-        after that with the final few giveaways being done in the following couple of days.
-        <br>
-        <small>
-          (The Project Blurple server is open all year though if you want to have some Blurple fun out of season).
-        </small>
-      </p>
+          <p>
+            Discord's birthday is on May 13th, so Project Blurple normally launches roughly a week before to get the
+            community hyped and ready to celebrate, with the main partying happening on May 13th. Things wrap up quickly
+            after that with the final few giveaways being done in the following couple of days.
+            <br>
+            <small>
+              (The Project Blurple server is open all year though if you want to have some Blurple fun out of season).
+            </small>
+          </p>
 
-      <p>
-        Project Blurple started in 2018 to celebrate Discord's 3rd birthday and has happened each year since, becoming
-        more organised and much larger each year. 2018 saw a massive initial splash with 15,000 Blurple users that
-        year and over 3,400 servers participating. 2019 was a bit quieter with only 4k Blurple users and just
-        over 400 servers involved.
-      </p>
+          <p>
+            Project Blurple started in 2018 to celebrate Discord's 3rd birthday and has happened each year since,
+            becoming more organised and much larger each year. 2018 saw a massive initial splash with 15,000 Blurple
+            users that year and over 3,400 servers participating. 2019 was a bit quieter with only 4k Blurple users and
+            just over 400 servers involved.
+          </p>
 
-      <blockquote>
-        <p>
-          We're back for 2020 and we're already on track to have the biggest event so far, with over 4.8k Blurple users
-          marking themselves as participating with a Blurple themed avatar in the first couple of days!
-        </p>
-      </blockquote>
+          <blockquote>
+            <p>
+              We're back for 2020 and we're already on track to have the biggest event so far, with over 4.8k Blurple
+              users marking themselves as participating with a Blurple themed avatar in the first couple of days!
+            </p>
+          </blockquote>
+        </div>
+        <div class="column is-one-quarter is-hidden-tablet-only is-hidden-mobile">
+          <ServerImages3 />
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+  import ServerImages3 from './images/servers3';
+
   export default {
     name: 'WhatIsProjectBlurple',
+    components: {
+      ServerImages3,
+    },
   };
 </script>
