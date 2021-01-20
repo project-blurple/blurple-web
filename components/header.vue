5aa7b4f50f5e47659014d22accfdcec83fba4171
MattIPv4 <me@mattcowley.co.uk>
2021-01-20 11:24:09 +0000

    Countdown for mobile viewports

--- components/header.vue
+++ components/header.vue
@@ -3,7 +3,7 @@
     <div :class="`hero-body${countdown ? ' has-countdown' : ''}`">
       <div class="container">
         <div v-if="countdown" class="countdown">
-          {{ countdownText }}
+          <span>{{ countdownText }}</span>
         </div>
         <article class="media">
           <div class="media-content">
