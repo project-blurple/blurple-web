8f2097342b38daf978d116cde945a85d9c5fb0a4
MattIPv4 <me@mattcowley.co.uk>
2021-05-07 13:15:19 +0100

    Add two more partners

--- components/images/servers3.vue
+++ components/images/servers3.vue
@@ -4,9 +4,15 @@
       <a href="https://discord.gg/cYakVbr" target="_blank" rel="noopener">
         <img src="../../assets/img/servers/car-crushers-official-discord.png" title="Car Crushers Official Discord" alt="">
       </a>
+      <a href="https://discord.gg/promote" target="_blank" rel="noopener">
+        <img src="../../assets/img/servers/advertise-your-server.png" title="Advertise Your Server" alt="">
+      </a>
       <!-- 3 more? -->
     </div>
     <div class="column is-half">
+      <a href="https://discord.gg/scpsl" target="_blank" rel="noopener">
+        <img src="../../assets/img/servers/scp-secret-laboratory.png" title="SCP: Secret Laboratory" alt="">
+      </a>
       <a href="https://discord.gg/pepe" target="_blank" rel="noopener">
         <img src="../../assets/img/servers/pepe-emoji-server.png" title="Pepe Emoji Server" alt="">
       </a>
