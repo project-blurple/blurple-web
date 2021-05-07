142198a63f9d93268594ac57c2cc8334eda6f402
MattIPv4 <me@mattcowley.co.uk>
2021-05-07 20:05:38 +0100

    Partner tweaks

--- components/images/servers3.vue
+++ components/images/servers3.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="columns images">
     <div class="column is-half">
+      <a href="https://discord.gg/kurzgesagt" target="_blank" rel="noopener">
+        <img
+          src="../../assets/img/servers/kurzgesagt-in-a-nutshell.png"
+          loading="lazy"
+          title="Kurzgesagt - In A Nutshell"
+          alt="Kurzgesagt - In A Nutshell"
+        >
+      </a>
+      <a href="https://discord.gg/swiss001" target="_blank" rel="noopener">
+        <img
+          src="../../assets/img/servers/swiss001.png"
+          loading="lazy"
+          title="Swiss001 Official Discord Server"
+          alt="Swiss001 Official Discord Server"
+        >
+      </a>
       <a href="https://discord.gg/tfQqub6" target="_blank" rel="noopener">
         <img
           src="../../assets/img/servers/discord-boats.png"
@@ -25,24 +41,16 @@
           alt="The Farming Community"
         >
       </a>
-      <a href="https://discord.gg/EcwPVjGy" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/the-nug-bucket.png"
-          loading="lazy"
-          title="The Nug Bucket"
-          alt="The Nug Bucket"
-        >
-      </a>
-      <a href="https://discord.gg/promote" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/advertise-your-server.png"
-          loading="lazy"
-          title="Advertise Your Server"
-          alt="Advertise Your Server"
-        >
-      </a>
     </div>
     <div class="column is-half">
+      <a href="https://discord.gg/jb" target="_blank" rel="noopener">
+        <img
+          src="../../assets/img/servers/r-jailbreak.png"
+          loading="lazy"
+          title="r/Jailbreak"
+          alt="r/Jailbreak"
+        >
+      </a>
       <a href="https://discord.gg/miaplays" target="_blank" rel="noopener">
         <img
           src="../../assets/img/servers/mia-plays.png"
@@ -65,14 +73,6 @@
           loading="lazy"
           title="Pepe Emoji Server"
           alt="Pepe Emoji Server"
-        >
-      </a>
-      <a href="https://discord.gg/EcwPVjGy" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/sleepy-club.png"
-          loading="lazy"
-          title="Sleepy Club"
-          alt="Sleepy Club"
         >
       </a>
       <a href="https://discord.gg/krunker" target="_blank" rel="noopener">
