d9b23ed86feacfe4cefbfa5dcf9a0d7f3eb87073
MattIPv4 <me@mattcowley.co.uk>
2021-05-13 17:43:24 +0100

    Updated stats

--- build/fetch_stats.js
+++ build/fetch_stats.js
@@ -4,13 +4,13 @@ import path from 'path';
 export default async () => {
   // TODO: Replace this with an API call?
   await fs.promises.writeFile(path.join(__dirname, 'data.json'), JSON.stringify({
-    members: 52332, // -serverinfo
-    blurple: 5158, // !roles -counts
-    servers: 883, // Search `in:#blurple-servers from: Blurplefied#5422`
-    messages: 233922, // Search `after: 2021-05-06 in: general in: bot-playground in: blurplefier-1 in: blurplefier-2 in: blurple-check in: canvas in: paint`
-    donators: 25, // !roles -counts
-    painters: 318, // !roles -counts
-    artists: 483, // !roles -counts
-    adventurers: 225, // !roles -counts
+    members: 70744, // -serverinfo
+    blurple: 9435, // !roles -counts
+    servers: 1396, // Search `in:#blurple-servers from: Blurplefied#5422`
+    messages: 527190, // Search `after: 2021-05-06 in: general in: bot-playground in: blurplefier-1 in: blurplefier-2 in: blurple-check in: canvas in: paint`
+    donators: 43, // !roles -counts
+    painters: 656, // !roles -counts
+    artists: 868, // !roles -counts
+    adventurers: 437, // !roles -counts
   }));
 };
