b5479b1457695be28db19aca292972eb59fc6f0f
MattIPv4 <me@mattcowley.co.uk>
2022-05-07 14:52:44 +0100

    Update third block of servers

--- components/images/servers3.vue
+++ components/images/servers3.vue
@@ -1,86 +1,38 @@
 <template>
   <div class="columns images">
     <div class="column is-half">
-      <a href="https://discord.gg/swiss001" target="_blank" rel="noopener">
+      <a href="https://discord.gg/langs" target="_blank" rel="noopener">
         <img
-          src="../../assets/img/servers/swiss001.png"
+          src="../../assets/img/servers/practice-your-language.png"
           loading="lazy"
-          title="Swiss001 Official Discord Server"
-          alt="Swiss001 Official Discord Server"
+          title="Practice Your Language"
+          alt="Practice Your Language"
         >
       </a>
-      <a href="https://discord.gg/tfQqub6" target="_blank" rel="noopener">
+      <a href="https://discord.gg/cYakVbr" target="_blank" rel="noopener">
         <img
-          src="../../assets/img/servers/discord-boats.png"
+          src="../../assets/img/servers/car-crushers-roblox-community.png"
           loading="lazy"
-          title="Discord Boats"
-          alt="Discord Boats"
-        >
-      </a>
-      <a href="https://discord.gg/starwars" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/r-starwars.png"
-          loading="lazy"
-          title="/r/StarWars"
-          alt="/r/StarWars"
-        >
-      </a>
-      <a href="https://discord.gg/farming" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/the-farming-community.png"
-          loading="lazy"
-          title="The Farming Community"
-          alt="The Farming Community"
-        >
-      </a>
-      <a href="https://discord.gg/imagine" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/pure-imagination.png"
-          loading="lazy"
-          title="Pure Imagination"
-          alt="Pure Imagination"
+          title="Car Crushers Roblox Community"
+          alt="Car Crushers Roblox Community"
         >
       </a>
     </div>
     <div class="column is-half">
-      <a href="https://discord.gg/jb" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/r-jailbreak.png"
-          loading="lazy"
-          title="r/Jailbreak"
-          alt="r/Jailbreak"
-        >
-      </a>
-      <a href="https://discord.gg/scpsl" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/scp-secret-laboratory.png"
-          loading="lazy"
-          title="SCP: Secret Laboratory"
-          alt="SCP: Secret Laboratory"
-        >
-      </a>
       <a href="https://discord.gg/pepe" target="_blank" rel="noopener">
         <img
-          src="../../assets/img/servers/pepe-emoji-server.png"
+          src="../../assets/img/servers/pepe-server.png"
           loading="lazy"
-          title="Pepe Emoji Server"
-          alt="Pepe Emoji Server"
+          title="Pepe Server"
+          alt="Pepe Server"
         >
       </a>
-      <a href="https://discord.gg/krunker" target="_blank" rel="noopener">
+      <a href="https://discord.gg/wonderlands" target="_blank" rel="noopener">
         <img
-          src="../../assets/img/servers/krunker-bunker.png"
+          src="../../assets/img/servers/tiny-tinas-wonderlands.png"
           loading="lazy"
-          title="Krunker Bunker"
-          alt="Krunker Bunker"
-        >
-      </a>
-      <a href="https://discord.gg/sound" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/sounds-world.png"
-          loading="lazy"
-          title="Sounds's World"
-          alt="Sounds's World"
+          title="Tiny Tina's Wonderlands"
+          alt="Tiny Tina's Wonderlands"
         >
       </a>
     </div>
