ab663c79c12b24fe08c26ff586d11cdd70c93836
MattIPv4 <me@mattcowley.co.uk>
2022-05-07 14:53:22 +0100

    Update fourth block of servers (empty)

--- components/images/servers4.vue
+++ components/images/servers4.vue
@@ -1,40 +1,8 @@
 <template>
   <div class="columns images is-light">
     <div class="column is-half">
-      <a href="https://discord.gg/photography" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/photography-lounge.png"
-          loading="lazy"
-          title="Photography Lounge"
-          alt="Photography Lounge"
-        >
-      </a>
-      <a href="https://discord.gg/yq2gFrnzjH" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/r-gabrieldropout.png"
-          loading="lazy"
-          title="/r/GabrielDropout"
-          alt="/r/GabrielDropout"
-        >
-      </a>
     </div>
     <div class="column is-half">
-      <a href="https://discord.gg/fkesy5Wv5v" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/insaneinthediscord.png"
-          loading="lazy"
-          title="insaneinthediscord"
-          alt="insaneinthediscord"
-        >
-      </a>
-      <a href="https://discord.gg/anigami" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/anigamis-tea-shop.png"
-          loading="lazy"
-          title="Anigami's Tea Shop"
-          alt="Anigami's Tea Shop"
-        >
-      </a>
     </div>
   </div>
 </template>
