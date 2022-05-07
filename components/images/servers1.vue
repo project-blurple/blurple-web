96fb7ccf30483aa461a00b0e974a2246b40b3dc3
MattIPv4 <me@mattcowley.co.uk>
2022-05-07 14:41:57 +0100

    Update first block of servers

--- components/images/servers1.vue
+++ components/images/servers1.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="columns images">
     <div class="column is-half">
-      <a href="https://discord.gg/VqnKpAg" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/ayana.png"
-          loading="lazy"
-          title="Ayana"
-          alt="Ayana"
-        >
-      </a>
-      <a href="https://discord.gg/ncEPCTC" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/dyno.png"
-          loading="lazy"
-          title="Dyno™"
-          alt="Dyno™"
-        >
-      </a>
-    </div>
-    <div class="column is-half">
-      <a href="https://discord.gg/borderlands" target="_blank" rel="noopener">
-        <img
-          src="../../assets/img/servers/borderlands.png"
-          loading="lazy"
-          title="Borderlands"
-          alt="Borderlands"
-        >
-      </a>
       <a href="https://discord.gg/tatsu" target="_blank" rel="noopener">
         <img
           src="../../assets/img/servers/tatsu-lounge.png"
           loading="lazy"
           title="Tatsu's Lounge"
           alt="Tatsu's Lounge"
+        >
+      </a>
+      <a href="https://discord.gg/imagine" target="_blank" rel="noopener">
+        <img
+          src="../../assets/img/servers/pure-imagination.png"
+          loading="lazy"
+          title="Pure Imagination"
+          alt="Pure Imagination"
+        >
+      </a>
+    </div>
+    <div class="column is-half">
+      <a href="https://discord.gg/VqnKpAg" target="_blank" rel="noopener">
+        <img
+          src="../../assets/img/servers/ayana.png"
+          loading="lazy"
+          title="Ayana Community"
+          alt="Ayana Community"
+        >
+      </a>
+      <a href="https://discord.gg/borderlands" target="_blank" rel="noopener">
+        <img
+          src="../../assets/img/servers/borderlands.png"
+          loading="lazy"
+          title="Borderlands"
+          alt="Borderlands"
         >
       </a>
     </div>
